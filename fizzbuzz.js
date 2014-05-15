ac4bc6f2da12f309beaffb2eabf6517076577a95
smuenzinger <sarah.muenzinger@gmail.com>
2014-05-15 14:07:49 -0400

    prompts and functions

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,25 +1,27 @@
 
 $(document).ready(function(){
-	for(i=1;i<=100;i++){
-		var isDivisableBy3 = (i%3===0);
-		var isDivisableBy5 = (i%5===0);
-
-		var output;
-
-		if(isDivisableBy3&&isDivisableBy5){
-			output = "FizzBuzz";
+	var questionNum = prompt("Pick a number for Fizzbuzz");
+	questionNum = +questionNum;
+	var isFizzBuzz = function(x) {
+		for(i=0;i<=x;i++){
+			var isDivisableBy3 = (i%3===0);
+			var isDivisableBy5 = (i%5===0);
+			var output;
+			if(isDivisableBy3&&isDivisableBy5){
+				output = "FizzBuzz";
+			}
+			else if(isDivisableBy3){
+				output = "Fizz";
+			}
+			else if(isDivisableBy5){
+				output = "Buzz";
+			}
+			else{
+				output = i;
+			}
+			$("body").append("<div>"+ output +"</div>");
 		}
-		else if(isDivisableBy3){
-			output = "Fizz";
-		}
-		else if(isDivisableBy5){
-			output = "Buzz";
-		}
-		else{
-			output = i;
-		}
-		$("body").append("<div>"+ output +"</div>");
-	}
-
+	};
+	isFizzBuzz(questionNum);
 });
 
