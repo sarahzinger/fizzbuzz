30ef0405da463859629531972359195f006e8f32
smuenzinger <sarah.muenzinger@gmail.com>
2014-05-15 14:30:23 -0400

    nan
    asks if it is a number first

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,25 +3,28 @@ $(document).ready(function(){
 	var questionNum = prompt("Pick a number for Fizzbuzz");
 	questionNum = +questionNum;
 	var isFizzBuzz = function(x) {
-		for(i=1;i<=x;i++){
-			var isDivisableBy3 = (i%3===0);
-			var isDivisableBy5 = (i%5===0);
-			var output;
-			if(isDivisableBy3&&isDivisableBy5){
-				output = "FizzBuzz";
+		if (isNaN(x)){
+			$("body").append("<div>"+ "You didn't pick a number, silly!"+"</div>");
+		} else {
+			for(i=1;i<=x;i++){
+				var isDivisableBy3 = (i%3===0);
+				var isDivisableBy5 = (i%5===0);
+				var output;
+				if(isDivisableBy3&&isDivisableBy5){
+					output = "FizzBuzz";
+				}
+				else if(isDivisableBy3){
+					output = "Fizz";
+				}
+				else if(isDivisableBy5){
+					output = "Buzz";
+				}
+				else{
+					output = i;
+				}
+				$("body").append("<div>"+ output +"</div>");
 			}
-			else if(isDivisableBy3){
-				output = "Fizz";
-			}
-			else if(isDivisableBy5){
-				output = "Buzz";
-			}
-			else{
-				output = i;
-			}
-			$("body").append("<div>"+ output +"</div>");
 		}
 	};
 	isFizzBuzz(questionNum);
 });
-
